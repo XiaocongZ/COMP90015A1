7e9366c13ef063067a851ccc20451ed050c7fc21
Cipher <invicible2010@gmail.com>
2022-09-11 21:46:46 +1000

    Search

--- src/main/java/comp90015/idxsrv/peer/Peer.java
+++ src/main/java/comp90015/idxsrv/peer/Peer.java
@@ -114,7 +114,43 @@ public class Peer implements IPeer {
 			InetAddress idxAddress,
 			int idxPort,
 			String idxSecret) {
-		tgui.logError("searchIdxServer unimplemented");
+
+		try {
+			Socket socket = new Socket(idxAddress, idxPort);
+			InputStream inputStream = socket.getInputStream();
+			OutputStream outputStream = socket.getOutputStream();
+			BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream, StandardCharsets.UTF_8));
+			BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(outputStream, StandardCharsets.UTF_8));
+
+			SearchRequest sReq = new SearchRequest(maxhits, keywords);
+
+			WelcomeMsg welcomeMsg = (WelcomeMsg) readMsg(bufferedReader);
+			tgui.logInfo("Server welcome: " + welcomeMsg.toString());
+
+			AuthenticateRequest aReq = new AuthenticateRequest(idxSecret);
+			writeMsg(bufferedWriter, aReq);
+			AuthenticateReply aRep = (AuthenticateReply) readMsg(bufferedReader);
+			if( !aRep.success){
+				throw new IOException("Authentication Failure");
+			}
+			writeMsg(bufferedWriter, sReq);
+			SearchReply sRep = (SearchReply) readMsg(bufferedReader);
+			if (sRep.hits < 1){
+				tgui.logInfo("No such file found.");
+			} else {
+				tgui.logInfo("Search success; numbers of seeders: " + sRep.seedCounts);
+			}
+
+		}
+		catch (IOException e){
+			tgui.logError("searchIdxServer: " + e.getMessage());
+		}
+		catch (NoSuchAlgorithmException e){
+			tgui.logError("searchIdxServer: " + e.getMessage());
+		} catch (JsonSerializationException e) {
+			tgui.logError("searchIdxServer: " + e.getMessage());
+		}
+
 	}
 
 	/**
